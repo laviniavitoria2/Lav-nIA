9e83b2da59dde0325fd2a2b3cc12d8489f1f1aaa
laviniavitoria2 <vitoria.ferraz15@icloud.com>
2026-04-14 04:36:47 -0400

    Atualizar o cli.py

--- lavnia/lavnia/cli.py
+++ lavnia/lavnia/cli.py
@@ -1,26 +1,8 @@
-import sys
-from lavnia.modules.chat import chat
-from lavnia.modules.peticao import gerar_peticao
+name: peticao_inicial
 
-def main():
-    comando = sys.argv[1] if len(sys.argv) > 1 else "help"
-
-    if comando == "chat":
-        chat()
-
-    elif comando == "peticao":
-        texto = input("Descreva o caso: ")
-        print(gerar_peticao(texto))
-
-    elif comando == "help":
-        print("""
-Comandos:
-lavnia chat
-lavnia peticao
-        """)
-
-    else:
-        print("Comando inválido")
-
-if __name__ == "__main__":
-    main()
+steps:
+  - classificar_caso
+  - buscar_modelo
+  - preencher_dados
+  - gerar_peticao
+  - revisar_texto
