1cdbe0605eeee8e8dfcddbcdfe5cb23c76fe1099
laviniavitoria2 <vitoria.ferraz15@icloud.com>
2026-04-14 03:57:27 -0400

    Atualizar o cli.py

--- lavnia/cli.py
+++ lavnia/cli.py
@@ -1,1 +1,11 @@
-lavnia/cli.py
+      chat():
+    print("🤖 Lav-nIA iniciada (digite 'sair' para encerrar)\n")
+
+     True:
+        msg input("Você: ")
+
+        msg.lower() "sair":
+            
+
+        resposta f"Resposta simulada para: {msg}"
+        print("IA:", resposta)
