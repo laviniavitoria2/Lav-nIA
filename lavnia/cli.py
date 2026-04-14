32a32418dbd4d1897225e77c90c819717a32e27a
laviniavitoria2 <vitoria.ferraz15@icloud.com>
2026-04-14 04:21:00 -0400

    Atualizar o cli.py

--- lavnia/cli.py
+++ lavnia/cli.py
@@ -1,11 +1,23 @@
-chat():
-    print("🤖 Lav-nIA iniciada (digite 'sair' para encerrar)\n")
+import sys
+from lavnia.modules.chat import chat
+from lavnia.modules.peticao import gerar_peticao
 
-     True:
-        msg input("Você: ")
+def main():
+    if len(sys.argv) < 2:
+        print("Use: lavnia [chat|peticao]")
+        return
 
-        msg.lower() "sair":
-            
+    comando = sys.argv[1]
 
-        resposta f"Resposta simulada para: {msg}"
-        print("IA:", resposta)
+    if comando == "chat":
+        chat()
+
+    elif comando == "peticao":
+        texto = input("Descreva o caso: ")
+        print(gerar_peticao(texto))
+
+    else:
+        print("Comando inválido")
+
+if __name__ == "__main__":
+    main()
